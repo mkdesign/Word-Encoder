66ae73952131df6de606d101aea51e69678903a2
mkdesign <me@mkdesign.ir>
2014-02-04 15:42:52 +0330

    Decode finished

--- scripts.js
+++ scripts.js
@@ -3,10 +3,22 @@ function encode(string){
 	var len = string.length;
 	var tmp,first, end;
 
-	first= string.charAt(0);
-	end= string.charAt(len-1);
-	tmp=string.substr(0,Math.ceil(len/2))+end+string.substr(Math.ceil(len/2))+first;
-	result.unshift(tmp);
+	first = string.charAt(0);
+	end   = string.charAt(len-1);
+	tmp   = string.substr(0,Math.ceil(len/2))+end+string.substr(Math.ceil(len/2))+first;
+	result.push(tmp);
+
+}
+
+function decode(string){
+	var len = string.length;
+	var tmp,mid;
+	mid= Math.ceil(len/2)-1;
+
+	end   = string.charAt(len-1);
+	tmp   = string.substr(0,mid)+string.slice(mid+1,-1);
+	result.push(tmp);
+	
 }
 
 window.onload= function(){
@@ -15,8 +27,8 @@ window.onload= function(){
 	var stack = new Array();
 	encodeBtn.onclick = function(){
 
-		var str= document.getElementById('str').value;
-		stack=str.split(" ");
+		var str = document.getElementById('str').value;
+		stack   = str.split(" ");
 		var length = stack.length;
 		for(i=0;i<length;i++){
 			encode(stack[i]);
@@ -24,6 +36,13 @@ window.onload= function(){
 		alert(result);
 	}
 	decodeBtn.onclick = function(){
+		var str = document.getElementById('str').value;
+		stack   = str.split(" ");
+		var length = stack.length;
+		for(i=0;i<length;i++){
+			decode(stack[i]);
+		}
+		alert(result);
 		
 	}
 
